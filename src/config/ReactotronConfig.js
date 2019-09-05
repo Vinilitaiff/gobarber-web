a6fded92aa3559f3908392698ed5d8b3081233be
vinilitaiff <vinilitaiff@gmail.com>
2019-09-05 00:53:40 -0400

    configurando rotas privadas da aplicacao

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,7 +1,9 @@
 import Reactotron from 'reactotron-react-js';
 
 if (process.env.NODE_ENV === 'development') {
-  const tron = Reactotron.configure().connect();
+  const tron = Reactotron.configure()
+    .configure({ host: '192.168.0.16' })
+    .connect();
 
   tron.clear();
 
