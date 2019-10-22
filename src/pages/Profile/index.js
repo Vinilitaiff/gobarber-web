b895eff68e7daba1183242854ca08f92d1a7ef35
vinilitaiff <vinilitaiff@gmail.com>
2019-10-22 00:26:15 -0400

    pagina de perfil e atuliazacao do perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector, useDispatch } from 'react-redux';
 
-// import { Container } from './styles';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereco de email" />
+
+        <hr />
+
+        <Input
+          name="oldPassword"
+          type="password"
+          placeholder="Seu senha atual"
+        />
+        <Input name="password" type="password" placeholder="Seu senha nova" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirmacao de senha"
+        />
+
+        <button type="submit">Atualizar Perfil</button>
+      </Form>
+
+      <button type="submit">Sair do GoBarber</button>
+    </Container>
+  );
 }
