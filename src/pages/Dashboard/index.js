cbc217e8884c841274f6f00fc00252ab88f0c618
vinilitaiff <vinilitaiff@gmail.com>
2019-10-29 21:43:40 -0400

    ajustes na comparacao de datas na listagem de agendamentos

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -12,7 +12,7 @@ import {
   parseISO,
 } from 'date-fns';
 import { utcToZonedTime } from 'date-fns-tz';
-import pt from 'date-fns/locale/pt-BR';
+import pt from 'date-fns/locale/pt';
 import api from '~/services/api';
 
 import { Container, Time } from './styles';
@@ -22,6 +22,7 @@ const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 export default function Dashboard() {
   const [schedule, setSchedule] = useState([]);
   const [date, setDate] = useState(new Date());
+
   const dateFormmatted = useMemo(
     () => format(date, "d 'de' MMMM", { locale: pt }),
     [date]
@@ -33,17 +34,19 @@ export default function Dashboard() {
         params: { date },
       });
 
-      const timezona = Intl.DateTimeFormat().resolvedOptions().timeZone;
+      const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
       const data = range.map(hour => {
         const checkDate = setSeconds(setMinutes(setHours(date, hour), 0), 0);
-        const compareDate = utcToZonedTime(checkDate, timezona);
+        // const checkDate = setHours(setMinutes(setSeconds(date, 0), 0), hour);
+        const compareDate = utcToZonedTime(checkDate, timezone);
 
         return {
           time: `${hour}:00h`,
           past: isBefore(compareDate, new Date()),
-          appointment: response.data.find(a =>
-            isEqual(parseISO(a.date), compareDate)
+          appointment: response.data.find(
+            a => parseISO(a.date).toString() === compareDate.toString()
+            // isEqual(parseISO(a.date), compareDate)
           ),
         };
       });
